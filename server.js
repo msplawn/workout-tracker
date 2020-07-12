2813cc0fe97a22614867a356ced359ecc4bdc1be
msplawn <mmsplawn@gmail.com>
2020-07-12 15:57:30 -0500

    added routes

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const htmlRoutes = require("./routes/html-routes.js");
+const apiRoutes = require("./routes/api-routes.js");
 
 const PORT = process.env.PORT || 3200;
 
@@ -12,6 +14,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
+app.use(htmlRoutes);
+app.use(apiRoutes);
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", { useNewUrlParser: true });
 
